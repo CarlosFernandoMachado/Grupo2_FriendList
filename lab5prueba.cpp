af476f81ba3b75c33fbda1a30cf7a84ce3f83740
christian-edgardo-rodriguez <christian.rodriguez@unitec.edu>
2016-02-26 15:57:06 -0600

    hice el resto de ncurses

--- lab5prueba.cpp
+++ lab5prueba.cpp
@@ -16,7 +16,8 @@ int main(int argc, char*argv[]){
 	char string[25];
 	print_in_middle(stdscr, LINES / 2, 0, 0, strcpy(string,"Bienvenido a tu agenda de amigos"));
 	print_in_middle(stdscr, LINES / 2+2, 0, 0, strcpy(string,"Laboratorio de Progra 3"));
-	if(getch()){
+	getch();
+	if(true){
 		clear();
 		if(has_colors() == FALSE){	
 			endwin();
@@ -46,9 +47,52 @@ int main(int argc, char*argv[]){
 		print_in_middle(stdscr, LINES / 2+3, 0, 0, strcpy(string,"3. Salir"));
 		attroff(COLOR_PAIR(4));
 
-	    getch();
-		endwin();
-	}			
+		char caracter= getch();
+		while(caracter=='1'||caracter=='2'){
+			if (caracter=='1'){
+				clear();
+				init_pair(5, COLOR_BLUE, COLOR_BLACK);
+				attron(COLOR_PAIR(5));
+				print_in_middle(stdscr, LINES / 2, 0, 0, strcpy(string,"Ingrese Nombre: "));
+				attroff(COLOR_PAIR(5));
+				getch();
+
+				init_pair(6, COLOR_GREEN, COLOR_BLACK);
+				attron(COLOR_PAIR(6));
+				print_in_middle(stdscr, LINES / 2+1, 0, 0, strcpy(string,"Ingrese Numero: "));
+				attroff(COLOR_PAIR(6));
+				getch();
+			}else if (caracter=='2'){
+				clear();
+				print_in_middle(stdscr, LINES / 2, 0, 0, strcpy(string,"Nombre "));
+				print_in_middle(stdscr, LINES / 2, 15, 0, strcpy(string,"Celular "));
+				getch();
+			}
+			clear();
+			refresh();
+			init_pair(1, COLOR_MAGENTA, COLOR_BLACK);
+			attron(COLOR_PAIR(1));
+			print_in_middle(stdscr, LINES / 2, 0, 0, strcpy(string,"------MENU--------"));
+			attroff(COLOR_PAIR(1));
+
+			init_pair(2, COLOR_BLUE, COLOR_BLACK);
+			attron(COLOR_PAIR(2));
+			print_in_middle(stdscr, LINES / 2+1, 0, 0, strcpy(string,"1. Agregar contacto"));
+			attroff(COLOR_PAIR(2));
+
+			init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+			attron(COLOR_PAIR(3));
+			print_in_middle(stdscr, LINES / 2+2, 0, 0, strcpy(string,"2. Agregar numero"));
+			attroff(COLOR_PAIR(3));
+
+			init_pair(4, COLOR_RED, COLOR_BLACK);
+			attron(COLOR_PAIR(4));
+			print_in_middle(stdscr, LINES / 2+3, 0, 0, strcpy(string,"3. Salir"));
+			attroff(COLOR_PAIR(4));
+			caracter= getch();
+		}
+	}
+	endwin();			
 	return 0;
 }
 void print_in_middle(WINDOW *win, int starty, int startx, int width, char* string){	
